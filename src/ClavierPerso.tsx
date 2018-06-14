c2b3e5ab2ab174cbb3a37f66004e520129a0b0cf
Julien Seng <julien.seng@insee.fr>
2018-06-14 14:42:39 +0200

    calcul pourcentage et remise à 0 à la saisie

--- src/ClavierPerso.tsx
+++ src/ClavierPerso.tsx
@@ -2,18 +2,29 @@ import * as React from "react";
 import Keyboard from "react-virtual-keyboard";
 
 // tslint:disable-next-line:no-empty-interface
-interface PropsInterface {}
+interface PropsInterface {
+  initialValue: string;
+}
 
 interface StateInterface {
+  prixInitial: string;
   input: string;
+  prix: string;
 }
 export default class ClavierPerso extends React.Component<
   PropsInterface,
   StateInterface
 > {
+  keyboard: Keyboard | null;
   constructor(props: PropsInterface) {
     super(props);
-    this.state = { input: "" };
+    this.state = {
+      input: this.props.initialValue,
+      prixInitial: this.props.initialValue,
+      prix: this.props.initialValue
+    };
+    this.onInputChange = this.onInputChange.bind(this);
+    this.beforeClose = this.beforeClose.bind(this);
   }
 
   beforeVisible(event: string | Event) {
@@ -22,13 +33,45 @@ export default class ClavierPerso extends React.Component<
 
   beforeClose(event: string | Event) {
     document.getElementsByName("keyboard")[0].removeAttribute("readOnly");
+    if (this.keyboard!.state.value === "") {
+      this.keyboard!.setState({ value: this.state.prixInitial });
+    }
   }
 
-  onInputChange(event?: string | Event) {
-    console.log("event", event);
+  onInputChange(
+    nouveauPrix?: string | Event,
+    keyboard?: Element,
+    el?: Element
+  ) {
+    console.log("Nouveau Prix: ", nouveauPrix);
+    console.log("prix initial: ", this.state.prixInitial);
+
+    if (typeof nouveauPrix === "string") {
+      if (nouveauPrix.includes("%")) {
+        const pourcentReduction = nouveauPrix
+          .replace(this.state.prix, "")
+          .replace("%", "")
+          .replace("-", "");
+        const prixMaj =
+          Number(this.state.prix) * (1 - Number(pourcentReduction) / 100);
+        const prixArrondi = Number.parseFloat(prixMaj.toString()).toFixed(2);
+        this.setState({ prix: prixArrondi, input: prixArrondi });
+      } else if (this.state.input === this.state.prixInitial) {
+        console.log("remise à 0");
+        this.setState({
+          prix: nouveauPrix.replace(this.state.prix, ""),
+          input: nouveauPrix.replace(this.state.prix, "")
+        });
+      } else {
+        this.setState({ prix: nouveauPrix, input: nouveauPrix });
+      }
+    }
+    this.keyboard!.setState({ value: this.state.prix });
   }
 
   public render() {
+    console.log("this.state.input ", this.state.input);
+
     return (
       <Keyboard
         value={this.state.input}
@@ -42,9 +85,9 @@ export default class ClavierPerso extends React.Component<
               "-90% -80% -70% -60%",
               "-50% -40% -35% -30%",
               "-25% -20% -15% -10%",
-              "{normal}"
+              "{normal} {a}"
             ],
-            normal: ["7 8 9", "4 5 6", "1 2 3", "0 . -", "{meta1} {b}"]
+            normal: ["7 8 9", "4 5 6", "1 2 3", "0 . -", "{meta1} {b} {a}"]
           },
           display: {
             meta1: "%",
@@ -59,10 +102,10 @@ export default class ClavierPerso extends React.Component<
           usePreview: false,
           useWheel: false,
           beforeVisible: this.beforeVisible,
-          beforeClose: this.beforeClose,
-          userClosed: false
+          beforeClose: this.beforeClose
         }}
         onChange={this.onInputChange}
+        ref={k => (this.keyboard = k)}
       />
     );
   }
